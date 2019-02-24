340a360c1a2908a3c571932a120447c2f3feb6d8
Blovien <rossidario07@gmail.com>
2019-02-24 22:05:30 +0100

    Add Maven support

--- src/main/java/com/polypenguin/pencil/engine/action/BlockChangeAction.java
+++ src/main/java/com/polypenguin/pencil/engine/action/BlockChangeAction.java
@@ -14,6 +14,9 @@ import java.util.ArrayList;
 public class BlockChangeAction implements PencilAction {
 
     private PencilPlayer player;
+    /**
+     * {@link PencilState} is missing maybe is {@link com.polypenguin.pencil.engine.utils.miscellaneous.PencilPreState} ?
+     */
     private ArrayList<PencilState> states;
     private int ID;
 
